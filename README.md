8e80e9a211e37d59ef1df1df253c356206ddcb33
kp <pancholikshitij94@gmail.com>
2026-06-13 18:31:20 -0700

    Initial commit: parametric ball bearing generator

---
+++ README.md
@@ -0,0 +1,7 @@
+# fusion-360-ball-bearing
+
+A parametric script for Autodesk Fusion 360 to model a complete ball bearing assembly consisting of:
+* Outer Housing (Ring)
+* Inner Housing (Ring)
+* Separator (with dynamic hole count optimization)
+* Cork / Cap Plug (with screwdriver slot)
---
+++ ballbearing.py
@@ -0,0 +1,301 @@
+import adsk.core, adsk.fusion, traceback
+import math
+import os
+
+HEIGHT = 0.6  # Height of the bearing in cm (6mm)
+ball_diameter = 0.45  # Ball diameter in cm (4.5mm)
+offset_towards_z = 0.1
+outer_housing_outer_dia = 3.0
+outer_housing_thickness = 0.25
+outer_housing_inner_dia = outer_housing_outer_dia - 2*outer_housing_thickness
+offset_for_revolte_cut = 0.1
+inner_housing_thickness = 0.2
+inner_housing_outer_dia = outer_housing_inner_dia - 0.3*2
+inner_housing_inner_dia = inner_housing_outer_dia - 2*inner_housing_thickness
+min_gap_between_separater_holes = 0.5
+separater_hole_diameter = ball_diameter - 0.2
+cork_hole_diameter = ball_diameter+0.07
+cork_cap_diameter = cork_hole_diameter-0.03
+
+def extrude_ring(comp, sketch, height):
+    # Find the ring profile (which has 2 loops)
+    prof = None
+    for p in sketch.profiles:
+        if p.profileLoops.count == 2:
+            prof = p
+            break
+    if not prof:
+        prof = sketch.profiles.item(0)
+        
+    # Extrude the ring profile
+    ext_in = comp.features.extrudeFeatures.createInput(prof, adsk.fusion.FeatureOperations.NewBodyFeatureOperation)
+    ext_in.setDistanceExtent(False, adsk.core.ValueInput.createByReal(height))
+    return comp.features.extrudeFeatures.add(ext_in)
+
+def create_common_outer_housing(design, name):
+    rootComp = design.rootComponent
+    
+    # Create new component
+    occ = rootComp.occurrences.addNewComponent(adsk.core.Matrix3D.create())
+    comp = occ.component
+    comp.name = name
+    
+    # Add sketch on XY plane
+    sk = comp.sketches.add(comp.xYConstructionPlane)
+    sk.name = name + " Sketch"
+    
+    sk.sketchCurves.sketchCircles.addByCenterRadius(adsk.core.Point3D.create(0,0,0), outer_housing_outer_dia / 2.0)
+    sk.sketchCurves.sketchCircles.addByCenterRadius(adsk.core.Point3D.create(0,0,0), outer_housing_inner_dia / 2.0)
+    
+    # Extrude the ring to HEIGHT
+    extrude_ring(comp, sk, HEIGHT)
+    
+    # Sketch on XZ plane for the revolute cut
+    sk_cut = comp.sketches.add(comp.xZConstructionPlane)
+    model_pt = adsk.core.Point3D.create(outer_housing_inner_dia / 2.0 - offset_towards_z, 0, HEIGHT / 2.0)
+    sketch_pt = sk_cut.modelToSketchSpace(model_pt)
+    sk_cut.sketchCurves.sketchCircles.addByCenterRadius(sketch_pt, ball_diameter / 2.0)
+    
+    # Revolve Cut around Z axis (restricted to local housing body)
+    prof_cut = sk_cut.profiles.item(0)
+    rev_in = comp.features.revolveFeatures.createInput(prof_cut, comp.zConstructionAxis, adsk.fusion.FeatureOperations.CutFeatureOperation)
+    rev_in.setAngleExtent(False, adsk.core.ValueInput.createByReal(math.pi * 2))
+    
+    # Restrict cut to only this component's body
+    body = comp.bRepBodies.item(0)
+    rev_in.participantBodies = [body]
+    
+    comp.features.revolveFeatures.add(rev_in)
+    
+    return comp
+
+def create_outer_housing(design):
+    comp = create_common_outer_housing(design, "Housing")
+    body = comp.bRepBodies.item(0)
+
+    # Create a tangent plane to cut the cork hole.
+    planes = comp.constructionPlanes
+    planeInput = planes.createInput()
+    planeInput.setByOffset(comp.yZConstructionPlane, adsk.core.ValueInput.createByReal(outer_housing_outer_dia / 2.0))
+    tangent_plane = planes.add(planeInput)
+    
+    # On this tanget plane create a sketch which is a circle of the size cork_hole_diameter
+    sk_tangent = comp.sketches.add(tangent_plane)
+    sk_tangent.name = "Tangent Sketch"
+    
+   
+    model_pt_tangent = adsk.core.Point3D.create(outer_housing_outer_dia / 2.0, 0, HEIGHT / 2.0)
+    sketch_pt_tangent = sk_tangent.modelToSketchSpace(model_pt_tangent)
+    sk_tangent.sketchCurves.sketchCircles.addByCenterRadius(sketch_pt_tangent, cork_hole_diameter / 2.0)
+    
+    # Extrude cut the cork hole
+    prof_tangent = sk_tangent.profiles.item(0)
+    ext_cut_in = comp.features.extrudeFeatures.createInput(prof_tangent, adsk.fusion.FeatureOperations.CutFeatureOperation)
+    distance_def = adsk.fusion.DistanceExtentDefinition.create(adsk.core.ValueInput.createByReal(outer_housing_thickness * 1.5))
+    ext_cut_in.setOneSideExtent(distance_def, adsk.fusion.ExtentDirections.NegativeExtentDirection)
+    
+    # Restrict cut to only outer housing body
+    ext_cut_in.participantBodies = [body]
+    
+    comp.features.extrudeFeatures.add(ext_cut_in)
+    
+    return comp
+
+def create_inner_housing(design):
+    rootComp = design.rootComponent
+    
+    # Create new component
+    occ = rootComp.occurrences.addNewComponent(adsk.core.Matrix3D.create())
+    comp = occ.component
+    comp.name = "Inner Housing"
+    
+    # Add sketch on XY plane
+    sk = comp.sketches.add(comp.xYConstructionPlane)
+    sk.name = "Inner Housing Sketch"
+    sk.sketchCurves.sketchCircles.addByCenterRadius(adsk.core.Point3D.create(0,0,0), inner_housing_outer_dia / 2.0)
+    
+    sk.sketchCurves.sketchCircles.addByCenterRadius(adsk.core.Point3D.create(0,0,0), inner_housing_inner_dia / 2.0)
+    
+    # Extrude the ring to HEIGHT
+    extrude_ring(comp, sk, HEIGHT)
+    
+    # Sketch on XZ plane for the revolute cut
+    sk_cut = comp.sketches.add(comp.xZConstructionPlane)
+
+    model_pt = adsk.core.Point3D.create(inner_housing_outer_dia / 2.0 + offset_for_revolte_cut, 0, HEIGHT / 2.0)
+    sketch_pt = sk_cut.modelToSketchSpace(model_pt)
+    sk_cut.sketchCurves.sketchCircles.addByCenterRadius(sketch_pt, ball_diameter / 2.0) # radius is ball_diameter / 2.0
+    
+    # Revolve Cut around Z axis (restricted to local inner housing body)
+    prof_cut = sk_cut.profiles.item(0)
+    rev_in = comp.features.revolveFeatures.createInput(prof_cut, comp.zConstructionAxis, adsk.fusion.FeatureOperations.CutFeatureOperation)
+    rev_in.setAngleExtent(False, adsk.core.ValueInput.createByReal(math.pi * 2))
+    
+    # Restrict cut to only this component's body
+    body = comp.bRepBodies.item(0)
+    rev_in.participantBodies = [body]
+    
+    comp.features.revolveFeatures.add(rev_in)
+    
+    return comp
+
+def compute_number_of_holes_in_separater(mid_dia):
+    # Constraint: Maximize the number of separator holes such that the centers
+    # are spaced so that the gap between any two adjacent holes of size
+    # 'ball_diameter' is at least 3mm (0.3cm).
+    circumference = math.pi * mid_dia
+    num_holes = int(circumference / (separater_hole_diameter + min_gap_between_separater_holes))
+    return num_holes
+
+def create_separator(design):
+    rootComp = design.rootComponent
+    
+    # Create new component
+    occ = rootComp.occurrences.addNewComponent(adsk.core.Matrix3D.create())
+    comp = occ.component
+    comp.name = "Separator"
+    
+    # Add sketch on XY plane
+    sk = comp.sketches.add(comp.xYConstructionPlane)
+    sk.name = "Separator Sketch"
+    
+    sep_thickness = 0.05  # 0.5mm thickness in cm
+    mid_dia = (outer_housing_inner_dia + inner_housing_outer_dia) / 2.0
+    
+    sep_outer_dia = mid_dia + sep_thickness / 2.0
+    sep_inner_dia = mid_dia - sep_thickness / 2.0
+    
+    sk.sketchCurves.sketchCircles.addByCenterRadius(adsk.core.Point3D.create(0,0,0), sep_outer_dia / 2.0)
+    sk.sketchCurves.sketchCircles.addByCenterRadius(adsk.core.Point3D.create(0,0,0), sep_inner_dia / 2.0)
+    
+    # Extrude the ring to HEIGHT
+    extrude_ring(comp, sk, HEIGHT)
+    
+    # Create tangent plane (parallel to Z axis, perpendicular to XY plane, tangent to outer separator face)
+    planes = comp.constructionPlanes
+    planeInput = planes.createInput()
+    planeInput.setByOffset(comp.yZConstructionPlane, adsk.core.ValueInput.createByReal(sep_outer_dia / 2.0))
+    tangent_plane = planes.add(planeInput)
+    
+    # Create sketch on tangent plane
+    sk_tangent = comp.sketches.add(tangent_plane)
+    sk_tangent.name = "Tangent Sketch"
+    
+    # Circle center at height/2 along Z axis, Y axis as 0
+    model_pt = adsk.core.Point3D.create(sep_outer_dia / 2.0, 0, HEIGHT / 2.0)
+    sketch_pt = sk_tangent.modelToSketchSpace(model_pt)
+    sk_tangent.sketchCurves.sketchCircles.addByCenterRadius(sketch_pt, separater_hole_diameter)
+    
+    # Extrude cut the circle into the separator (towards the Z-axis / inwards)
+    prof_tangent = sk_tangent.profiles.item(0)
+    ext_cut_in = comp.features.extrudeFeatures.createInput(prof_tangent, adsk.fusion.FeatureOperations.CutFeatureOperation)
+    distance_def = adsk.fusion.DistanceExtentDefinition.create(adsk.core.ValueInput.createByReal(sep_thickness * 1.5))
+    ext_cut_in.setOneSideExtent(distance_def, adsk.fusion.ExtentDirections.NegativeExtentDirection)
+    
+    # Restrict cut to only separator body
+    body = comp.bRepBodies.item(0)
+    ext_cut_in.participantBodies = [body]
+    
+    ext_cut = comp.features.extrudeFeatures.add(ext_cut_in)
+    
+    num_holes = compute_number_of_holes_in_separater(mid_dia)
+    
+    # Circular Pattern to replicate the hole
+    objCollection = adsk.core.ObjectCollection.create()
+    for face in ext_cut.faces:
+        objCollection.add(face)
+        
+    pattern_in = comp.features.circularPatternFeatures.createInput(objCollection, comp.zConstructionAxis)
+    pattern_in.quantity = adsk.core.ValueInput.createByReal(num_holes)
+    pattern_in.computeOption = adsk.fusion.PatternComputeOptions.IdenticalPatternCompute
+    comp.features.circularPatternFeatures.add(pattern_in)
+    
+    return comp
+
+def create_cork(design):
+    comp = create_common_outer_housing(design, "Cork")
+    body = comp.bRepBodies.item(0)
+    
+    # Create tangent plane (parallel to Z axis, perpendicular to XY plane, tangent to outer housing face)
+    planes = comp.constructionPlanes
+    planeInput = planes.createInput()
+    planeInput.setByOffset(comp.yZConstructionPlane, adsk.core.ValueInput.createByReal(outer_housing_outer_dia / 2.0))
+    tangent_plane = planes.add(planeInput)
+    
+    # Create sketch on tangent plane
+    sk_tangent = comp.sketches.add(tangent_plane)
+    sk_tangent.name = "Tangent Sketch"
+    
+    # Circle center at height/2 along Z axis, Y axis as 0
+    model_pt_tangent = adsk.core.Point3D.create(outer_housing_outer_dia / 2.0, 0, HEIGHT / 2.0)
+    sketch_pt_tangent = sk_tangent.modelToSketchSpace(model_pt_tangent)
+    sk_tangent.sketchCurves.sketchCircles.addByCenterRadius(sketch_pt_tangent, cork_cap_diameter / 2.0)
+    
+    # Extrude intersect the circle into the housing (towards the Z-axis / inwards)
+    prof_tangent = sk_tangent.profiles.item(0)
+    ext_intersect_in = comp.features.extrudeFeatures.createInput(prof_tangent, adsk.fusion.FeatureOperations.IntersectFeatureOperation)
+    distance_def = adsk.fusion.DistanceExtentDefinition.create(adsk.core.ValueInput.createByReal(outer_housing_thickness * 1.5))
+    ext_intersect_in.setOneSideExtent(distance_def, adsk.fusion.ExtentDirections.NegativeExtentDirection)
+    
+    # Restrict intersect to only cork body
+    ext_intersect_in.participantBodies = [body]
+    
+    comp.features.extrudeFeatures.add(ext_intersect_in)
+    
+    # Create a horizontal slot cut (screwdriver slot) on the outer face of the cork
+    # We create a new sketch on the tangent plane
+    sk_slot = comp.sketches.add(tangent_plane)
+    sk_slot.name = "Slot Sketch"
+    
+    slot_width = 0.4  # 4mm horizontal length
+    slot_thickness = 0.08  # 0.8mm vertical thickness
+    
+    model_center = adsk.core.Point3D.create(outer_housing_outer_dia / 2.0, 0, HEIGHT / 2.0)
+    sketch_center = sk_slot.modelToSketchSpace(model_center)
+    
+    x_c = sketch_center.x
+    y_c = sketch_center.y
+    
+    # Define rectangle corners relative to local sketch space
+    p1 = adsk.core.Point3D.create(x_c - slot_width/2.0, y_c - slot_thickness/2.0, 0)
+    p2 = adsk.core.Point3D.create(x_c + slot_width/2.0, y_c - slot_thickness/2.0, 0)
+    p3 = adsk.core.Point3D.create(x_c + slot_width/2.0, y_c + slot_thickness/2.0, 0)
+    p4 = adsk.core.Point3D.create(x_c - slot_width/2.0, y_c + slot_thickness/2.0, 0)
+    
+    sk_slot.sketchCurves.sketchLines.addByTwoPoints(p1, p2)
+    sk_slot.sketchCurves.sketchLines.addByTwoPoints(p2, p3)
+    sk_slot.sketchCurves.sketchLines.addByTwoPoints(p3, p4)
+    sk_slot.sketchCurves.sketchLines.addByTwoPoints(p4, p1)
+    
+    prof_slot = sk_slot.profiles.item(0)
+    
+    # Extrude cut the slot inwards by 0.5 mm (0.03 cm)
+    ext_slot_in = comp.features.extrudeFeatures.createInput(prof_slot, adsk.fusion.FeatureOperations.CutFeatureOperation)
+    distance_def_slot = adsk.fusion.DistanceExtentDefinition.create(adsk.core.ValueInput.createByReal(0.05))
+    ext_slot_in.setOneSideExtent(distance_def_slot, adsk.fusion.ExtentDirections.NegativeExtentDirection)
+    
+    # Restrict cut to only cork body (after the intersect, there is only one body representing the cork)
+    cork_body = comp.bRepBodies.item(0)
+    ext_slot_in.participantBodies = [cork_body]
+    
+    comp.features.extrudeFeatures.add(ext_slot_in)
+    
+    return comp
+
+def run(context):
+    ui = None
+    try:
+        app = adsk.core.Application.get()
+        ui  = app.userInterface
+        design = adsk.fusion.Design.cast(app.activeProduct)
+        if not design:
+            ui.messageBox('No active Fusion design', 'No Design')
+            return
+
+        create_outer_housing(design)
+        create_inner_housing(design)
+        create_separator(design)
+        create_cork(design)
+    except:
+        if ui:
+            ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
